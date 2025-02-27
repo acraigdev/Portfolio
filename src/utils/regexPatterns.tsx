4d562820e7e5172813b91b914f332384d89914f9
Allie C <alexandriamcraig@gmail.com>
2025-02-26 17:24:49 -0700

    Fix name regex

--- src/utils/regexPatterns.tsx
+++ src/utils/regexPatterns.tsx
@@ -1,3 +1,3 @@
-export const NAME_PATTERN = /^[A-Za-z0-9]{1,100}$/;
+export const NAME_PATTERN = /^[A-Za-z0-9 ]{1,100}$/;
 export const EMAIL_PATTERN = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const MESSAGE_PATTERN = /^.{1,1024}$/;
